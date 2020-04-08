b5639353372d7c3d2edaa903b89cd1dda2680e94
jdspell <spelljd15@gmail.com>
2020-04-08 07:54:38 -0500

    added error enum

--- networking/encoding/ParamterEncoding.swift
+++ networking/encoding/ParamterEncoding.swift
@@ -3,3 +3,9 @@ public typealias Parameters = [String:Any]
 public protocol ParameterEncoder {
     static func encode(urlRequest: inout URLRequest, with paramters: Parameters) throws
 }
+
+public enum NetworkError: String, Error {
+    case parametersNil = "Parameters were nil"
+    case encodingFailed = "Parameter encoding failed"
+    case missingURL = "URL is nil"
+}
