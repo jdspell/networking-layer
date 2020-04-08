d8e61f17016a3547e5cc0621a0a7129e8f78dbc0
jdspell <spelljd15@gmail.com>
2020-04-07 21:21:29 -0500

    protocol for endpoint configuration

---
+++ networking/service/EndPointType.swift
@@ -0,0 +1,8 @@
+// information for configuring an endpoint
+protocol EndPointType {
+    var baseURL: URL { get }
+    var path: String { get }
+    var httpMethod: HTTPMethod { get }
+    var task: HTTPTask { get }
+    var headers: HTTPHreaders? { get }    
+}
